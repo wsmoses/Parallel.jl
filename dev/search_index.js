b6de1cb760f6db93c8104f01d74e73653e50679e
Documenter.jl <documenter@juliadocs.github.io>
2022-03-13 03:07:23 +0000

    build based on 54a9cac

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = Parallel","category":"page"},{"location":"#Parallel","page":"Home","title":"Parallel","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for Parallel.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [Parallel]","category":"page"}]
+}
